d9a11b583a47a2013bb1195cd304e2d94de51d7c
goooooooofy <goofy1163191060@gmail.com>
2015-08-19 21:59:54 +0800

    location and View LocationName

---
+++ GaoDeMapTest/AMapDemoSwift-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  AMapDemoSwift-Bridging-Header.h
+//  GaoDeMapTest
+//
+//  Created by goofygao on 8/19/15.
+//  Copyright (c) 2015 goofyy. All rights reserved.
+//
+
+#ifndef GaoDeMapTest_AMapDemoSwift_Bridging_Header_h
+#define GaoDeMapTest_AMapDemoSwift_Bridging_Header_h
+
+#import <MAMapKit/MAMapKit.h>
+#import <AMapSearchKit/AMapSearchAPI.h>
+
+#endif
